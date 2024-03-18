faf6cb8452aae7119d12f446f75d886d6b8f1278
Gauravjot Garaya <gauravjot@outlook.com>
2024-03-18 03:10:33 -0700

    added folder functionality and sorting

--- public/js/script.js
+++ public/js/script.js
@@ -61,10 +61,21 @@ function hideSidebar() {
 // Highlight the current page in the sidebar
 const sidebarDocList = document
 	.getElementById("sidebar-doc-list")
-	.getElementsByTagName("li");
+	.getElementsByTagName("a");
 for (let i = 0; i < sidebarDocList.length; i++) {
 	if (sidebarDocList[i].id === getLastURLPart()) {
 		sidebarDocList[i].setAttribute("aria-current", "page");
+		let parent = sidebarDocList[i].parentElement;
+		while (parent.id !== "sidebar-doc-list") {
+			// if tagName is button, then it is an accordion
+			if (parent.tagName === "DIV" && parent.classList.contains("accordion")) {
+				console.log(parent);
+				parent.setAttribute("aria-expanded", "true");
+			}
+
+			parent = parent.parentElement;
+		}
+		break;
 	}
 }
 function getLastURLPart() {
@@ -73,6 +84,22 @@ function getLastURLPart() {
 		return window.location.href.split("/").slice(-2)[0];
 	}
 	return part;
+}
+
+// Sidebar accordions
+const sidebarAccordions = document.getElementsByClassName("accordion");
+for (let i = 0; i < sidebarAccordions.length; i++) {
+	// Get first element of the accordion
+	const button = sidebarAccordions[i].getElementsByTagName("button")[0];
+	button.addEventListener("click", function (e) {
+		e.preventDefault();
+		button.parentElement.setAttribute(
+			"aria-expanded",
+			button.parentElement.getAttribute("aria-expanded") === "true"
+				? "false"
+				: "true"
+		);
+	});
 }
 
 // Get all headings under the element with id `md-content`
@@ -107,29 +134,32 @@ function giveMarginForHeading(li, heading) {
 	}
 }
 // Highlight the current heading in toc on page scroll
-const tocLinks = document.getElementById("toc").getElementsByTagName("a");
-window.addEventListener("scroll", () => {
-	// If mobile, return
-	if (window.matchMedia("only screen and (max-width: 1024px)").matches) {
-		return;
-	}
-	let latest;
-	let fromTop = window.scrollY;
-	for (let i = 0; i < mdHeadingList.length; i++) {
-		let section = document.getElementById(mdHeadingList[i].href.split("#")[1]);
-		// When heading is halfway from top, highlight the toc link
-		if (
-			section.offsetTop <= fromTop + screen.height / 2 &&
-			section.offsetTop + section.offsetHeight > fromTop + screen.height / 2
-		) {
-			// Remove if new heading is selected
-			latest = tocLinks[i];
+let tocLinks = document.getElementById("toc");
+if (typeof tocLinks !== "undefined" && tocLinks !== null) {
+	tocLinks = tocLinks.getElementsByTagName("a");
+	window.addEventListener("scroll", () => {
+		// If mobile, return
+		if (window.matchMedia("only screen and (max-width: 1024px)").matches) {
+			return;
 		}
-	}
-	if (latest) {
-		for (let i = 0; i < tocLinks.length; i++) {
-			tocLinks[i].setAttribute("aria-current", "false");
+		let latest;
+		let fromTop = window.scrollY;
+		for (let i = 0; i < mdHeadingList.length; i++) {
+			let section = document.getElementById(mdHeadingList[i].href.split("#")[1]);
+			// When heading is halfway from top, highlight the toc link
+			if (
+				section.offsetTop <= fromTop + screen.height / 2 &&
+				section.offsetTop + section.offsetHeight > fromTop + screen.height / 2
+			) {
+				// Remove if new heading is selected
+				latest = tocLinks[i];
+			}
 		}
-		latest.setAttribute("aria-current", "true");
-	}
-});
+		if (latest) {
+			for (let i = 0; i < tocLinks.length; i++) {
+				tocLinks[i].setAttribute("aria-current", "false");
+			}
+			latest.setAttribute("aria-current", "true");
+		}
+	});
+}
--- src/server.js
+++ src/server.js
@@ -23,15 +23,15 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 const matter = require("gray-matter");
 
 app.get("/:article", (req, res) => {
+	let basedir = path.join(__dirname, "..", "content");
+	const subpath = req.params.article.split("___");
+	const filename = subpath.pop().replaceAll("__", " ") + ".md";
+	for (let i = 0; i < subpath.length; i++) {
+		basedir = path.join(basedir, subpath[i].replaceAll("__", " "));
+	}
+	const filepath = path.join(basedir, filename);
 	// read the markdown file
-	const file = matter.read(
-		path.join(
-			__dirname,
-			"..",
-			"content",
-			req.params.article.replaceAll("_", " ") + ".md"
-		)
-	);
+	const file = matter.read(filepath);
 
 	// use markdown-it to convert content to HTML
 	const md = require("markdown-it")();
@@ -45,29 +45,136 @@ app.get("/:article", (req, res) => {
 	const content = file.content;
 	const result = md.render(content);
 
-	const docs = fs
-		.readdirSync(path.join(__dirname, "..", "content"))
-		.filter((file) => file.endsWith(".md") && !file.startsWith("_"));
+	const docs = getFiles(path.join(__dirname, "..", "content"));
 
 	res.render("doc", {
 		post: result,
 		title: file.data.title,
 		date: file.data.date,
 		description: file.data.description,
-		docs: docs,
+		docs: generateNestedListHTML(docs),
 		current: req.params.article,
 	});
 });
 
 app.get("/", (req, res) => {
-	const docs = fs
-		.readdirSync(path.join(__dirname, "..", "content"))
-		.filter((file) => file.endsWith(".md") && !file.startsWith("_"));
+	const docs = getFiles(path.join(__dirname, "..", "content"));
 	res.render("index", {
-		docs: docs,
+		docs: generateNestedListHTML(docs),
 	});
 });
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
+
+// Get all md files from the content folder and the subfolders.
+// We will store the subfolder name as the category of the article.
+function getFiles(dir, basedir, files_, category) {
+	files_ = files_ || [];
+	category = category || "content";
+	basedir = basedir || dir;
+	const files = fs.readdirSync(dir);
+	for (var i in files) {
+		let name = dir + "/" + files[i];
+		if (fs.statSync(name).isDirectory()) {
+			getFiles(name, basedir, files_, files[i]);
+		} else {
+			if (!files[i].endsWith(".md") || files[i].startsWith("_")) {
+				continue;
+			}
+			files_.push({
+				path: name,
+				title:
+					matter.read(path.resolve(name)).data.title ||
+					files[i].replace(".md", ""),
+				category: name.replace(basedir, "").replace(files[i], ""),
+			});
+		}
+	}
+	// sort based on the category and then the title
+	files_.sort((a, b) => {
+		if (a.category < b.category) {
+			return -1;
+		}
+		if (a.category > b.category) {
+			return 1;
+		}
+		if (a.title < b.title) {
+			return -1;
+		}
+		if (a.title > b.title) {
+			return 1;
+		}
+		return 0;
+	});
+	return files_;
+}
+
+function generateNestedListHTML(data) {
+	// Group items by category
+	const groupedItems = {};
+	const categories = [];
+	data.forEach((item) => {
+		const category = item.category;
+		if (!groupedItems[category]) {
+			groupedItems[category] = [];
+			categories.push(category);
+		}
+		groupedItems[category].push(item);
+	});
+
+	// Function to recursively build the nested UL list
+	function buildNestedListHTML(category_index) {
+		const category = categories[category_index];
+		let html = "";
+		let id = category
+			.replace(/^\/|\/$/g, "")
+			.replaceAll("/", "___")
+			.replaceAll(" ", "__");
+		if (category === "/") {
+			html = "<ul>";
+			id = "";
+		} else {
+			html = '<div class="accordion" id="' + id + '">';
+			html +=
+				'<button class="accordion__button"><span>' +
+				id.split("___").slice(-1).join("").replaceAll("__", " ") +
+				"</span></button>";
+			html += "<ul><div>";
+		}
+		groupedItems[category].forEach((item) => {
+			// match item.title with [digit+]_[w+] pattern
+			const match = item.title.match(/^(\d+_)/);
+			const link =
+				id +
+				(id.length > 0 ? "___" : "") +
+				item.path.split("/").pop().replaceAll(" ", "__").replace(".md", "");
+			html += "<li>";
+			html +=
+				'<a id="' +
+				link +
+				'" href="/' +
+				link +
+				'">' +
+				(match ? item.title.replace(match[0], "") : item.title) +
+				"</a>";
+			if (
+				category_index + 1 < categories.length &&
+				categories[category_index + 1].startsWith(category)
+			) {
+				html += buildNestedListHTML(category_index + 1);
+			}
+			html += "</li>";
+		});
+		if (category === "/") {
+			html += "</ul>";
+		} else {
+			html += "</div></ul>";
+			html += "</div>";
+		}
+		return html;
+	}
+	let res = buildNestedListHTML(0);
+	return res;
+}
